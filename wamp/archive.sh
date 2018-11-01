5ec8ce07a0631f90f94f39a5109471e81acd4297
miroslav.kozel <mirek.kozel@gmail.com>
2018-11-01 12:05:02 +0100

    crossbar 1.0.0

---
+++ wamp/archive.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker save --output ../docker-image-archives/crossbar.tar crossbar:latest
---
+++ wamp/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t crossbar:latest -t crossbar:1.0.0 .
