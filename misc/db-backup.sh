952514999a22b9d882528d287c4f55e511f18279
miroslav.kozel <mirek.kozel@gmail.com>
2019-07-30 17:39:03 +0200

    build system without 'latest'

--- misc/db-backup.sh
+++ misc/db-backup.sh
@@ -8,6 +8,7 @@ db_default_list=( chameleon availability )
 backup_dir=db-backup
 
 # DESTINATION DB + CREDENTIALS
+mongo_version=3
 mongo_host=srv-mongo01.upp.cz:27017
 mongo_user=admin
 mongo_pass=m0ng0_01_adm1n
@@ -30,7 +31,7 @@ echo "Backup destination folder: ${backup_dir_abs}/${today}"
 for db in "${db_list[@]}"
 do
     echo "Backing up DB: ${db}..."
-    docker run --name mongodb -it --rm -v ${backup_dir_abs}:/data/backup/ chameleon/mongodb:latest bash -c "exec mongodump -h ${mongo_host} -u ${mongo_user} -p ${mongo_pass} --authenticationDatabase admin -d ${db} --out /data/backup/${today}"
+    docker run --name mongodb -it --rm -v ${backup_dir_abs}:/data/backup/ chameleon/mongodb:${mongo_version} bash -c "exec mongodump -h ${mongo_host} -u ${mongo_user} -p ${mongo_pass} --authenticationDatabase admin -d ${db} --out /data/backup/${today}"
 done
 
 echo "Done!"
